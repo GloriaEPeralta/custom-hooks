92fedf4b9522f0b02b2ddc467267d74d66c309f8
GloriaEPeralta <gloria.pp@culiacan.tecnm.mx>
2024-11-04 14:48:11 -0700

    useTodos agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,29 @@
+// es una función pura de JavaScript 
+
+
+
+export const todoReducer = (inicialState = [], action) => {
+
+    switch (action.type) {
+        case 'Add Todo':
+            return [...inicialState, action.payload];
+           
+        case 'Delete Todo':
+            return inicialState.filter(todo => todo.id !== action.payload); 
+        case 'Toggle Todo':
+            return inicialState.map(todo => {
+
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+
+            });
+        default:
+           return inicialState;
+    }  
+
+}
